dae332563724aee8bffc9f7f67f226165b632697
Theodor Emanuelsson <theodor.emanuelsson@me.com>
2022-04-19 14:08:09 +0200

    Attention Unet

---
+++ unet_attent.py
@@ -0,0 +1,238 @@
+# 3D Unet imported from:
+# Slightly adapted
+"""
+@author: Sreenivas Bhattiprolu
+Converted my standard 2D Unet to 3D. 
+"""
+
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import *
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.metrics import MeanIoU
+from tensorflow.keras.backend import int_shape, repeat_elements
+import tensorflow as tf
+
+kernel_initializer =  'he_uniform' #Try others if you want
+
+
+################################################################
+def attention_unet_model(IMG_HEIGHT, IMG_WIDTH, IMG_DEPTH, IMG_CHANNELS, num_classes):
+#Build the model
+    inputs = Input((IMG_HEIGHT, IMG_WIDTH, IMG_DEPTH, IMG_CHANNELS))
+    s = inputs
+
+    ###Contraction path
+    
+    ## GoDown 1
+    c1 = Conv3D(16, (3, 3, 3), activation='relu', kernel_initializer=kernel_initializer, padding='same')(s)
+    c1 = Dropout(0.1)(c1)
+    c1 = Conv3D(16, (3, 3, 3), activation='relu', kernel_initializer=kernel_initializer, padding='same')(c1)
+    p1 = MaxPooling3D((2, 2, 2))(c1)
+    
+    ## C1 goes to U9
+    
+    ## GoDown 2
+    c2 = Conv3D(32, (3, 3, 3), activation='relu', kernel_initializer=kernel_initializer, padding='same')(p1)
+    c2 = Dropout(0.1)(c2)
+    c2 = Conv3D(32, (3, 3, 3), activation='relu', kernel_initializer=kernel_initializer, padding='same')(c2)
+    p2 = MaxPooling3D((2, 2, 2))(c2)
+    ## C2 goes to U8
+ 
+    ## GoDown 3 
+    c3 = Conv3D(64, (3, 3, 3), activation='relu', kernel_initializer=kernel_initializer, padding='same')(p2)
+    c3 = Dropout(0.2)(c3)
+    c3 = Conv3D(64, (3, 3, 3), activation='relu', kernel_initializer=kernel_initializer, padding='same')(c3)
+    p3 = MaxPooling3D((2, 2, 2))(c3)
+    # C3 goes to U7
+     
+    ## GoDown 4 
+    c4 = Conv3D(128, (3, 3, 3), activation='relu', kernel_initializer=kernel_initializer, padding='same')(p3)
+    c4 = Dropout(0.2)(c4)
+    c4 = Conv3D(128, (3, 3, 3), activation='relu', kernel_initializer=kernel_initializer, padding='same')(c4)
+    p4 = MaxPooling3D(pool_size=(2, 2, 2))(c4)
+    # C4 goes to U6
+    
+    
+    ## Bridge
+    c5 = Conv3D(256, (3, 3, 3), activation='relu', kernel_initializer=kernel_initializer, padding='same')(p4)
+    c5 = Dropout(0.3)(c5)
+    c5 = Conv3D(256, (3, 3, 3), activation='relu', kernel_initializer=kernel_initializer, padding='same')(c5)
+    
+    # Attention gate 1
+    gate_0 = AttnGatingBlock(c4, c5, 16*16)
+    
+    #Expansive path 
+    ## GoUp 1
+    u6 = Conv3DTranspose(128, (2, 2, 2), strides=(2, 2, 2), padding='same')(c5)
+    u6 = concatenate([u6, gate_0])
+    c6 = Conv3D(128, (3, 3, 3), activation='relu', kernel_initializer=kernel_initializer, padding='same')(u6)
+    c6 = Dropout(0.2)(c6)
+    c6 = Conv3D(128, (3, 3, 3), activation='relu', kernel_initializer=kernel_initializer, padding='same')(c6)
+    
+    # Attention gate 2
+    gate_1 = AttnGatingBlock(c3, c6, 16*8)
+    
+    ## GoUp 2
+    u7 = Conv3DTranspose(64, (2, 2, 2), strides=(2, 2, 2), padding='same')(c6)
+    u7 = concatenate([u7, gate_1])
+    c7 = Conv3D(64, (3, 3, 3), activation='relu', kernel_initializer=kernel_initializer, padding='same')(u7)
+    c7 = Dropout(0.2)(c7)
+    c7 = Conv3D(64, (3, 3, 3), activation='relu', kernel_initializer=kernel_initializer, padding='same')(c7)
+    
+    gate_2 = AttnGatingBlock(c2, c7, 16*4)
+    
+    ## GoUp 3
+    u8 = Conv3DTranspose(32, (2, 2, 2), strides=(2, 2, 2), padding='same')(c7)
+    u8 = concatenate([u8, gate_2])
+    c8 = Conv3D(32, (3, 3, 3), activation='relu', kernel_initializer=kernel_initializer, padding='same')(u8)
+    c8 = Dropout(0.1)(c8)
+    c8 = Conv3D(32, (3, 3, 3), activation='relu', kernel_initializer=kernel_initializer, padding='same')(c8)
+    
+    gate_3 = AttnGatingBlock(c1, c8, 16*2)
+    
+    ## GoUp 4
+    u9 = Conv3DTranspose(16, (2, 2, 2), strides=(2, 2, 2), padding='same')(c8)
+    u9 = concatenate([u9, gate_3])
+    c9 = Conv3D(16, (3, 3, 3), activation='relu', kernel_initializer=kernel_initializer, padding='same')(u9)
+    c9 = Dropout(0.1)(c9)
+    c9 = Conv3D(16, (3, 3, 3), activation='relu', kernel_initializer=kernel_initializer, padding='same')(c9)
+     
+    outputs = Conv3D(num_classes, (1, 1, 1), activation='softmax')(c9)
+     
+    model = Model(inputs=[inputs], outputs=[outputs])
+    #compile model outside of this function to make it flexible. 
+    #model.summary()
+    
+    return model
+    
+def simple_unet_model(IMG_HEIGHT, IMG_WIDTH, IMG_DEPTH, IMG_CHANNELS, num_classes):
+#Build the model
+    inputs = Input((IMG_HEIGHT, IMG_WIDTH, IMG_DEPTH, IMG_CHANNELS))
+    #s = Lambda(lambda x: x / 255)(inputs)   #No need for this if we normalize our inputs beforehand
+    s = inputs
+
+    #Contraction path
+    c1 = Conv3D(16, (3, 3, 3), activation='relu', kernel_initializer=kernel_initializer, padding='same')(s)
+    c1 = Dropout(0.1)(c1)
+    c1 = Conv3D(16, (3, 3, 3), activation='relu', kernel_initializer=kernel_initializer, padding='same')(c1)
+    p1 = MaxPooling3D((2, 2, 2))(c1)
+    
+    c2 = Conv3D(32, (3, 3, 3), activation='relu', kernel_initializer=kernel_initializer, padding='same')(p1)
+    c2 = Dropout(0.1)(c2)
+    c2 = Conv3D(32, (3, 3, 3), activation='relu', kernel_initializer=kernel_initializer, padding='same')(c2)
+    p2 = MaxPooling3D((2, 2, 2))(c2)
+     
+    c3 = Conv3D(64, (3, 3, 3), activation='relu', kernel_initializer=kernel_initializer, padding='same')(p2)
+    c3 = Dropout(0.2)(c3)
+    c3 = Conv3D(64, (3, 3, 3), activation='relu', kernel_initializer=kernel_initializer, padding='same')(c3)
+    p3 = MaxPooling3D((2, 2, 2))(c3)
+     
+    c4 = Conv3D(128, (3, 3, 3), activation='relu', kernel_initializer=kernel_initializer, padding='same')(p3)
+    c4 = Dropout(0.2)(c4)
+    c4 = Conv3D(128, (3, 3, 3), activation='relu', kernel_initializer=kernel_initializer, padding='same')(c4)
+    p4 = MaxPooling3D(pool_size=(2, 2, 2))(c4)
+     
+    c5 = Conv3D(256, (3, 3, 3), activation='relu', kernel_initializer=kernel_initializer, padding='same')(p4)
+    c5 = Dropout(0.3)(c5)
+    c5 = Conv3D(256, (3, 3, 3), activation='relu', kernel_initializer=kernel_initializer, padding='same')(c5)
+    
+    #Expansive path 
+    u6 = Conv3DTranspose(128, (2, 2, 2), strides=(2, 2, 2), padding='same')(c5)
+    u6 = concatenate([u6, c4])
+    c6 = Conv3D(128, (3, 3, 3), activation='relu', kernel_initializer=kernel_initializer, padding='same')(u6)
+    c6 = Dropout(0.2)(c6)
+    c6 = Conv3D(128, (3, 3, 3), activation='relu', kernel_initializer=kernel_initializer, padding='same')(c6)
+     
+    u7 = Conv3DTranspose(64, (2, 2, 2), strides=(2, 2, 2), padding='same')(c6)
+    u7 = concatenate([u7, c3])
+    c7 = Conv3D(64, (3, 3, 3), activation='relu', kernel_initializer=kernel_initializer, padding='same')(u7)
+    c7 = Dropout(0.2)(c7)
+    c7 = Conv3D(64, (3, 3, 3), activation='relu', kernel_initializer=kernel_initializer, padding='same')(c7)
+     
+    u8 = Conv3DTranspose(32, (2, 2, 2), strides=(2, 2, 2), padding='same')(c7)
+    u8 = concatenate([u8, c2])
+    c8 = Conv3D(32, (3, 3, 3), activation='relu', kernel_initializer=kernel_initializer, padding='same')(u8)
+    c8 = Dropout(0.1)(c8)
+    c8 = Conv3D(32, (3, 3, 3), activation='relu', kernel_initializer=kernel_initializer, padding='same')(c8)
+     
+    u9 = Conv3DTranspose(16, (2, 2, 2), strides=(2, 2, 2), padding='same')(c8)
+    u9 = concatenate([u9, c1])
+    c9 = Conv3D(16, (3, 3, 3), activation='relu', kernel_initializer=kernel_initializer, padding='same')(u9)
+    c9 = Dropout(0.1)(c9)
+    c9 = Conv3D(16, (3, 3, 3), activation='relu', kernel_initializer=kernel_initializer, padding='same')(c9)
+     
+    outputs = Conv3D(num_classes, (1, 1, 1), activation='softmax')(c9)
+     
+    model = Model(inputs=[inputs], outputs=[outputs])
+    
+    return model
+    
+def expend_as(tensor, rep):
+
+    # Anonymous lambda function to expand the specified axis by a factor of argument, rep.
+    # If tensor has shape (512,512,N), lambda will return a tensor of shape (512,512,N*rep), if specified axis=2
+
+    my_repeat = Lambda(lambda x, repnum: repeat_elements(x, repnum, axis=4),
+                       arguments={'repnum': rep})(tensor)
+    return my_repeat    
+    
+def AttnGatingBlock(x, g, inter_shape):
+
+    shape_x = int_shape(x)
+    shape_g = int_shape(g)
+
+    # Getting the gating signal to the same number of filters as the inter_shape
+    phi_g = Conv3D(filters=inter_shape,
+                   kernel_size=1,
+                   strides=1,
+                   padding='same')(g)
+
+    # Getting the x signal to the same shape as the gating signal
+    theta_x = Conv3D(filters=inter_shape,
+                     kernel_size=3,
+                     strides=(shape_x[1] // shape_g[1],
+                              shape_x[2] // shape_g[2],
+                              shape_x[3] // shape_g[3]),
+                     padding='same')(x)
+
+    # Element-wise addition of the gating and x signals
+    add_xg = tf.keras.layers.add([phi_g, theta_x])
+    add_xg = Activation('relu')(add_xg)
+
+    # 1x1x1 convolution
+    psi = Conv3D(filters=1, kernel_size=1, padding='same')(add_xg)
+    psi = Activation('sigmoid')(psi)
+    shape_sigmoid = int_shape(psi)
+
+    # Upsampling psi back to the original dimensions of x signal
+    upsample_sigmoid_xg = UpSampling3D(
+        size=(shape_x[1] // shape_sigmoid[1], shape_x[2] // shape_sigmoid[2],
+              shape_x[3] // shape_sigmoid[3]))(psi)
+
+    # Expanding the filter axis to the number of filters in the original x signal
+    upsample_sigmoid_xg = expend_as(upsample_sigmoid_xg, shape_x[4])
+
+    # Element-wise multiplication of attention coefficients back onto original x signal
+    attn_coefficients = multiply([upsample_sigmoid_xg, x])
+
+    # Final 1x1x1 convolution to consolidate attention signal to original x dimensions
+    output = Conv3D(filters=shape_x[4],
+                    kernel_size=1,
+                    strides=1,
+                    padding='same')(attn_coefficients)
+    output = BatchNormalization()(output)
+    return output
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+    
+    
+    
